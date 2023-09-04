7e1bd6c880b9beb4a39475d2dc10e37013d16e26
Colas Vincent <60921938+vinctigra@users.noreply.github.com>
2023-09-04 21:02:40 +0200

    refacto

--- front/src/components/jurys/JuryDisplay.tsx
+++ front/src/components/jurys/JuryDisplay.tsx
@@ -1,45 +1,30 @@
 import {useEffect, useState} from "react";
-import {ethers} from "ethers";
-import contractsInterface from "../../contracts/contracts.ts";
-import {provider} from "../../provider/providers.ts";
 import {ipfsGetContent} from "../common/ipfs.ts";
 import {toString as uint8ArrayToString} from "uint8arrays/to-string";
 
 interface JuryDisplayProps {
-    tokenId: number
+    tokenURI: string
 }
-const JuryDisplay = ({tokenId}: JuryDisplayProps) => {
+const JuryDisplay = ({tokenURI}: JuryDisplayProps) => {
     const [juryImage, setJuryImage] = useState('');
 
     useEffect(() => {
-        if (tokenId ) {
+        if (tokenURI ) {
             (async () => {
-                let tokenUri: string = '';
-                const contract = new ethers.Contract(contractsInterface.contracts.Jurys.address, contractsInterface.contracts.Jurys.abi, provider);
+                const metadataString = await ipfsGetContent(tokenURI)
+                const data = JSON.parse(uint8ArrayToString(metadataString, 'utf8'))
 
-                try {
-                    tokenUri = await contract.tokenURI(tokenId);
-                }
-                catch (e) {
-                    console.log(e);
-                }
-
-                if (tokenUri) {
-                    const metadataString = await ipfsGetContent(tokenUri)
-                    const data = JSON.parse(uint8ArrayToString(metadataString, 'utf8'))
-
-                    if (data.image) {
-                        const imageContent = await ipfsGetContent(data.attributes[0].value)
-                        setJuryImage(uint8ArrayToString(imageContent, 'base64'))
-                    }
+                if (data.image) {
+                    const imageContent = await ipfsGetContent(data.attributes[0].value)
+                    setJuryImage(uint8ArrayToString(imageContent, 'base64'))
                 }
             })()
         }
-    }, [juryImage, setJuryImage, tokenId])
+    }, [juryImage, setJuryImage, tokenURI])
 
     return (
         <div style={{margin: 'auto', width: 200}}>
-            {tokenId > 0 && juryImage &&
+            {tokenURI && juryImage &&
                 <div>
                     <h3>Apercu :</h3>
                     <img src={`data:image/*;base64,${juryImage}`} alt="card" style={{height: '200px'}}/>
